2186b917577bbdece45aa0c6d6e1dacc208a2cd9
Urvashi Chhabra <urvashi.chhabra@mintworxs.com>
2023-09-19 14:26:45 +0530

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactamplified": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
